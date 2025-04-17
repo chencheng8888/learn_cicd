9c629ca28cc17a70f64c9b2f17b4c35c22410b32
chencheng8888 <2963801553@qq.com>
2025-04-17 11:01:22 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package learncicd
+
+import "github.com/gin-gonic/gin"
+
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/hello",func(ctx *gin.Context) {
+		ctx.JSON(200,gin.H{
+			"message":"Hello World",
+		})
+	})
+}
